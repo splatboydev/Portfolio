2cfe428e25472daf76903673c59db31f83090ca8
splatboydev <splatboy20081@gmail.com>
2023-01-14 14:40:11 +0100

    Android, apple favicon etc

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -14,7 +14,11 @@ class PortfolioDocument extends Document {
            href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@700&display=swap"
             rel="stylesheet"
           />
-          <link rel="shortcut icon" href="/static/favicon.ico"></link>
+          <link rel="shortcut icon" href="/static/favicon.ico" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/static/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png" />
+          <link rel="manifest" href="/static/site.webmanifest" />
           <script async src="https://arc.io/widget.min.js#dPRGqGD4"></script>
         </Head>
         <body>
