746949bf30cf40a887524d4d80986949091345f4
splatboydev <splatboy20081@gmail.com>
2023-01-14 14:09:13 +0100

    Allow arc to use a service worker

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,26 @@ import "../styles/style.css";
 import type { AppProps } from "next/app";
 import store from "../redux/store";
 import { Provider } from "react-redux";
+import { useEffect } from "react";
 
 const App = ({ Component, pageProps }: AppProps) => {
+  useEffect(() => {
+    try{
+    window.addEventListener("load", function () {
+      navigator.serviceWorker.register("../../public/arc-sw.js").then(
+        function (registration) {
+          console.log("Service Worker registration successful with scope: ", registration.scope);
+        },
+        function (err) {
+          console.log("Service Worker registration failed: ", err);
+        }
+      );
+    });
+  } catch (err) {
+    console.error(err);
+  }
+  }, [])
+
   return (
     <Provider store={store}>
       <Component {...pageProps} />
