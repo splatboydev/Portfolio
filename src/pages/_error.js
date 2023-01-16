c0fe55ef5361dc4d469dde09f7970e20b6e6f05e
splatboydev <splatboy20081@gmail.com>
2023-01-16 18:05:41 +0100

    Fix next warnings @ vercel

--- src/pages/_error.js
+++ src/pages/_error.js
@@ -8,7 +8,7 @@ function Error({ statusCode }) {
   )
 }
 
-Error.getInitialProps = ({ res, err }) => {
+Error.getStaticProps = ({ res, err }) => {
   const statusCode = res ? res.statusCode : err ? err.statusCode : 404
   return { statusCode }
 }
