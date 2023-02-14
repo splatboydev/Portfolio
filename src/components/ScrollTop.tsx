85e69697ae15612414e9913269d7b9f059f0bbfe
SplatboyDev <64037464+splatboydev@users.noreply.github.com>
2023-02-14 02:03:58 +0100

    Button transition (p1)

--- src/components/ScrollTop.tsx
+++ src/components/ScrollTop.tsx
@@ -4,7 +4,7 @@ import { HiArrowNarrowUp } from "react-icons/hi";
 const ScrollTop: FC = () => {
   return (
     <a
-      className="fixed right-12 bottom-12 md:right-20 md:bottom-16 p-3 md:p-4 bg-bg-surface rounded-full border-blue-500 border"
+      className="fixed right-12 bottom-12 md:right-20 md:bottom-16 p-3 md:p-4 bg-bg-surface rounded-full border-blue-500 border transition-all"
       href="#"
     >
       <HiArrowNarrowUp size={30} color="#C3C1BA" />
